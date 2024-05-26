cbf9add3a97d53aa564f99c4f2e9618e3b9cf51e
Abhi29999 <52719515+Abhi29999@users.noreply.github.com>
2024-05-26 15:12:25 -0700

    Create executeautopr.py

---
+++ executeautopr.py
@@ -0,0 +1,2 @@
+#execute the above code with input variables
+create_pr_disk_expansion(OLD_DISK_SIZE='2000',NEW_DISK_SIZE='4000',BRANCH_NAME='ds-12356-new-test-auto-pr-1',TARGET_FOLDER='C:/Users/abhi.lalwani/testPRauto4/root-mssql/prod/regions/us-central1/environments/p/compute/sql/f/fcsag03')
